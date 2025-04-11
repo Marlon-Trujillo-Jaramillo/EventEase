71319c82457486c0adadfab384decf67f3a607b5
Marlon-Trujillo-Jaramillo <118902850+Marlon-Trujillo-Jaramillo@users.noreply.github.com>
2025-04-11 14:17:55 -0500

    Subir el proyecto al repositorio

---
+++ EventEase app/Models/EventModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EventEase.Models
+{
+    public class EventModel
+    {
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        [Required(ErrorMessage = "Location is required")]
+        public string Location { get; set; }
+
+        public string Description { get; set; }
+    }
+}
